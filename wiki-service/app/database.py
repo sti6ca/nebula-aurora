ee9f76d864971af19ae34b57d0593e1dca329a43
STI6CA <tsilva.abm@gmail.com>
2025-11-29 02:13:48 -0300

    adapt database env var

--- wiki-service/app/database.py
+++ wiki-service/app/database.py
@@ -3,12 +3,9 @@ from sqlalchemy.orm import DeclarativeBase
 import os
 
 # Get Database URL from environment variable
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql+asyncpg://wiki_user:wiki_password@localhost:5432/wiki_db"
-)
-#if not DATABASE_URL:
-#    raise ValueError("DATABASE_URL environment variable is not set.")
+DATABASE_URL = os.getenv("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable is not set.")
 
 # Create async engine
 engine = create_async_engine(
